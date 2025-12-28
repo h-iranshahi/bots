dd11aada90d8af0d83f29dd6e4667eb71f0014ed
H.Iranshahi <h.iranshahi67@gmail.com>
2025-12-29 00:27:07 +0330

    telegram routes as api

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TelegramController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// telegram webhook
+Route::post('/telegram/webhook/{bot}', [TelegramController::class, 'updateMessages'])->name('telegram.updateMessages');
+Route::get('/telegram/set-webhook/{bot}', [TelegramController::class, 'setupWebhook']);
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TelegramController;
 
-Route::post('/telegram/webhook/{bot}', [TelegramController::class, 'updateMessages'])->name('telegram.updateMessages');
-Route::get('/telegram/set-webhook/{bot}', [TelegramController::class, 'setupWebhook']);
 
 Route::get('/', fn() => 'It works!');
